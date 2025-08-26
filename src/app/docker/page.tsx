474d36b87b828c24885eb65fae68a503a54d2b8c
WathsalaSanda <22106532@students.latrobe.edu.au>
2025-08-27 05:27:19 +1000

    docker

---
+++ src/app/docker/page.tsx
@@ -0,0 +1,8 @@
+export default function DockerPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Docker</h1>
+      <p>Dockerization notes / screenshots will go here.</p>
+    </main>
+  );
+}
